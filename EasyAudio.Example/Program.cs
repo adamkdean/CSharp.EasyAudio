6f68be4b9d4e9ebda6ad8df73ff2cd849b8b6a75
Adam K Dean <adamkdean@googlemail.com>
2013-06-26 14:56:40 +0100

    Added example use of SineWave class

--- EasyAudio.Example/Program.cs
+++ EasyAudio.Example/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace EasyAudio.Example
 {
@@ -10,6 +8,29 @@ namespace EasyAudio.Example
     {
         static void Main(string[] args)
         {
+            var list = new List<Tuple<float, float>>();
+            list.Add(new Tuple<float, float>(100f, 0.5f));
+            list.Add(new Tuple<float, float>(200f, 0.5f));
+            list.Add(new Tuple<float, float>(300f, 0.5f));
+            list.Add(new Tuple<float, float>(400f, 0.5f));
+            list.Add(new Tuple<float, float>(500f, 0.5f));
+            list.Add(new Tuple<float, float>(600f, 0.5f));
+            list.Add(new Tuple<float, float>(700f, 0.5f));
+            list.Add(new Tuple<float, float>(800f, 0.5f));
+            list.Add(new Tuple<float, float>(900f, 0.5f));
+            list.Add(new Tuple<float, float>(1000f, 0.5f));
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Playing tone at {0} hz for {1} seconds...", item.Item1, item.Item2);
+                using (var sw = new SineWave(item.Item1, item.Item2))
+                {
+                    sw.PlayOnce(item.Item2);
+                }
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
         }
     }
 }
