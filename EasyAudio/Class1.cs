7fae83085f5842663d1333ee196a8ae02e4174fb
Adam K Dean <adamkdean@googlemail.com>
2013-06-26 13:39:14 +0100

    Added class library

---
+++ EasyAudio/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyAudio
+{
+    public class Class1
+    {
+    }
+}
