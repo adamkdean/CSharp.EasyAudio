d1dc8d2b7fd954fb17c6de2c1e5748bd7187f893
Adam K Dean <adamkdean@googlemail.com>
2013-06-26 14:56:01 +0100

    Added SineWave capability

---
+++ EasyAudio/SineWave.cs
@@ -0,0 +1,64 @@
+﻿using NAudio.Wave;
+using System;
+using System.Timers;
+using System.Threading;
+
+namespace EasyAudio
+{
+    public class SineWave : IDisposable
+    {
+        private SineWaveProvider32 sineWave;
+        private WaveOut waveOut;
+
+        public float Frequency
+        {
+            get { return sineWave.Frequency; }
+            set { sineWave.Frequency = value; }
+        }
+
+        public float Amplitude
+        {
+            get { return sineWave.Amplitude; }
+            set { sineWave.Amplitude = value; }
+        }
+        
+        public SineWave(float frequency, float amplitude = 0.25f, int sampleRate = 16000, int channels = 1)
+        {
+            sineWave = new SineWaveProvider32(frequency, amplitude, sampleRate, channels);
+            waveOut = new WaveOut();
+            waveOut.Init(sineWave);
+        }
+
+        public void PlayOnce(float duration)
+        {
+            waveOut.Play();
+            Thread.Sleep((int)(duration * 1000f));
+            waveOut.Stop();
+        }
+
+        public void PlayOnceAsync(float duration)
+        {
+            waveOut.Play();
+            var timer = new System.Timers.Timer { Interval = (duration * 1000f) };
+            timer.Elapsed += new ElapsedEventHandler((o, e) => waveOut.Stop());
+            timer.Elapsed += new ElapsedEventHandler((o, e) => timer.Enabled = false);
+            timer.Enabled = true;
+        }
+
+        public void Play()
+        {
+            waveOut.Play();
+        }
+
+        public void Stop()
+        {
+            waveOut.Stop();
+        }
+
+        public void Dispose()
+        {            
+            waveOut.Dispose();
+            waveOut = null;
+        }
+    }    
+}
---
+++ EasyAudio/SineWaveProvider32.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace EasyAudio
+{
+    public class SineWaveProvider32 : WaveProvider32
+    {
+        private int sample = 0;
+
+        public float Frequency { get; set; }
+        public float Amplitude { get; set; }
+
+        public SineWaveProvider32(float frequency, float amplitude = 0.25f, int sampleRate = 16000, int channels = 1)
+        {
+            Frequency = frequency;
+            Amplitude = amplitude;
+            this.SetWaveFormat(sampleRate, channels);
+        }
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            int sampleRate = WaveFormat.SampleRate;
+            for (int n = 0; n < sampleCount; n++)
+            {
+                buffer[n + offset] = (float)(Amplitude * Math.Sin((2 * Math.PI * sample * Frequency) / sampleRate));
+                sample++;
+                if (sample >= sampleRate) sample = 0;
+            }
+            return sampleCount;
+        }
+    }
+}
